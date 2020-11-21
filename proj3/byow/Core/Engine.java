921e47ce9a1195c39e64448a2c94a2719f83535d
Joshua Delrosario <joshua.d.delrosario@gmail.com>
2020-11-21 15:40:42 -0800

    Engine and Player updates

--- proj3/byow/Core/Engine.java
+++ proj3/byow/Core/Engine.java
@@ -1,17 +1,35 @@
 package byow.Core;
 
+
 import byow.TileEngine.TERenderer;
 import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+import edu.princeton.cs.algs4.StdDraw;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.PrintWriter;
+import java.util.HashMap;
 import java.util.Random;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Engine {
     /* Feel free to change the width and height. */
     public static final int WIDTH = 80;
     public static final int HEIGHT = 30;
-    private TETile[][] worldFrame;
+    public TETile[][] worldFrame;
     private TERenderer renderer;
     Random r;
+    private BSP map;
+    private Player p;
+
+
+
+
+    //public Player p;
 
     public Engine() {
         worldFrame = new TETile[WIDTH][HEIGHT];
@@ -47,7 +65,8 @@ public class Engine {
      */
 
 
-    public TETile[][] interactWithInputString(String input) {
+    public TETile[][] interactWithInputString(String input)
+    throws Exception {
         // passed in as an argument, and return a 2D tile representation of the
         // world that would have been drawn if the same inputs had been given
         // to interactWithKeyboard().
@@ -61,7 +80,7 @@ public class Engine {
         if (input == null) {
             return null;
         }
-        System.out.println("Generating world with seed " + input);
+
         while (index < input.length()) {
             if (input.charAt(index) == 'N' || input.charAt(index) == 'n') {
                 index++;
@@ -72,12 +91,26 @@ public class Engine {
                 }
                 index++;
                 r  = new Random(seed);
-                worldFrame = new BSP(WIDTH, HEIGHT, r).createLeaves();
+                map = new BSP(WIDTH, HEIGHT, r);
+                worldFrame = map.createLeaves();
+                p = new Player(WIDTH, HEIGHT, worldFrame, r);
+                worldFrame = p.addPlayer();
+            } else if (input.charAt(index) == 'W' || input.charAt(index) == 'w'
+                    || input.charAt(index) == 'S' || input.charAt(index) == 's'
+                    || input.charAt(index) == 'A' || input.charAt(index) == 'a'
+                    || input.charAt(index) == 'D' || input.charAt(index) == 'd') {
+                worldFrame = p.move(input.charAt(index));
+                index++;
             } else if (input.charAt(index) == 'l' || input.charAt(index) == 'L') {
+                load();
                 index++;
-            } else if (input.charAt(index) == ':' && (input.charAt(index) == 'q'
-                    || input.charAt(index) == 'Q')) {
+            } else if (input.charAt(index) == 'S' || input.charAt(index) == 's') {
+                save();
                 index++;
+            } else if (input.charAt(index) == ':' && (input.charAt(index + 1) == 'q'
+                    || input.charAt(index + 1) == 'Q')) {
+                save();
+                break;
             } else {
                 index++;
             }
@@ -87,8 +120,12 @@ public class Engine {
     }
 
 
-    private TETile[][] newWorld(TETile[][] world) {
-        return world;
+    private TETile[][] getWorldFrame() {
+        return worldFrame;
+    }
+
+    private void setWorldFrame(TETile[][] world) {
+        worldFrame = world;
     }
 
     public void initializeRenderer() {
@@ -99,11 +136,111 @@ public class Engine {
         renderer.renderFrame(worldFrame);
     }
 
-    public static void main(String[] args) {
+    public void move(char c) {
+        worldFrame = p.move(c);
+    }
+
+    private TETile[][] load()
+            throws Exception {
+        File inputFile = new File("fa20-proj3-g488\\proj3\\byow\\Core\\saved.txt");
+        Scanner reader = new Scanner(inputFile);
+        p = new Player(WIDTH, HEIGHT, worldFrame, new Random());
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                if (reader.hasNextInt()) {
+                    int num = reader.nextInt();
+                    if (num == 0) {
+                        worldFrame[i][j] = Tileset.AVATAR;
+                        p.setX(i);
+                        p.setY(j);
+                    } else if (num == 1) {
+                        worldFrame[i][j] = Tileset.WALL;
+                    } else if (num == 2) {
+                        worldFrame[i][j] = Tileset.FLOOR;
+                    } else if (num == 3) {
+                        worldFrame[i][j] = Tileset.NOTHING;
+                    } else if (num == 4) {
+                        worldFrame[i][j] = Tileset.GRASS;
+                    } else if (num == 5) {
+                        worldFrame[i][j] = Tileset.WATER;
+                    } else if (num == 6) {
+                        worldFrame[i][j] = Tileset.FLOWER;
+                    } else if (num == 7) {
+                        worldFrame[i][j] = Tileset.LOCKED_DOOR;
+                    } else if (num == 8) {
+                        worldFrame[i][j] = Tileset.UNLOCKED_DOOR;
+                    } else if (num == 9) {
+                        worldFrame[i][j] = Tileset.SAND;
+                    } else if (num == 10) {
+                        worldFrame[i][j] = Tileset.MOUNTAIN;
+                    } else if (num == 11) {
+                        worldFrame[i][j] = Tileset.TREE;
+                    }
+                }
+            }
+        }return worldFrame;
+    }
+
+    public void save()
+            throws Exception {
+        File outputFile = new File("fa20-proj3-g488\\proj3\\byow\\Core\\saved.txt");
+        if (!outputFile.createNewFile()) {
+            outputFile.delete();
+            outputFile.createNewFile();
+        }
+        PrintWriter writer = new PrintWriter(outputFile);
+        for (int i = 0; i < WIDTH; i++) {
+            for (int j = 0; j < HEIGHT; j++) {
+                if (worldFrame[i][j].equals(Tileset.AVATAR)) {
+                    writer.write("0 ");
+                } else if (worldFrame[i][j].equals(Tileset.WALL)) {
+                    writer.write("1 ");
+                } else if (worldFrame[i][j].equals(Tileset.FLOOR)) {
+                    writer.write("2 ");
+                } else if (worldFrame[i][j].equals(Tileset.NOTHING)) {
+                    writer.write("3 ");
+                } else if (worldFrame[i][j].equals(Tileset.GRASS)) {
+                    writer.write("4 ");
+                } else if (worldFrame[i][j].equals(Tileset.WATER)) {
+                    writer.write("5 ");
+                } else if (worldFrame[i][j].equals(Tileset.FLOWER)) {
+                    writer.write("6 ");
+                } else if (worldFrame[i][j].equals(Tileset.LOCKED_DOOR)) {
+                    writer.write("7 ");
+                } else if (worldFrame[i][j].equals(Tileset.UNLOCKED_DOOR)) {
+                    writer.write("8 ");
+                } else if (worldFrame[i][j].equals(Tileset.SAND)) {
+                    writer.write("9 ");
+                } else if (worldFrame[i][j].equals(Tileset.MOUNTAIN)) {
+                    writer.write("10 ");
+                } else if (worldFrame[i][j].equals(Tileset.TREE)) {
+                    writer.write("11 ");
+                }
+            }
+        }
+        writer.close();
+    }
+
+    public static void main(String[] args)
+            throws Exception {
         Engine engine = new Engine();
         engine.initializeRenderer();
-        engine.interactWithInputString("N" + Math.abs(new Random().nextInt()) + "S");
+        //engine.interactWithInputString("N19980711S:q");
+        engine.load();
         engine.renderWorld();
+//        //engine.save();
+
+        char input = 0;
+        while (input != 'q') {
+            if (StdDraw.hasNextKeyTyped()) {
+                input = StdDraw.nextKeyTyped();
+                engine.move(input);
+                engine.renderWorld();
+            }
+            Thread.sleep(500);
+        }
+        engine.save();
         System.out.println("done");
+        return;
     }
 }
---
+++ proj3/byow/Core/Player.java
@@ -0,0 +1,75 @@
+package byow.Core;
+
+import byow.TileEngine.TETile;
+import byow.TileEngine.Tileset;
+
+import java.awt.event.KeyEvent;
+import java.util.Random;
+
+public class Player {
+    Random r;
+    private int initialX, initialY, x, y,
+            worldWidth, worldHeight;
+    private TETile[][] worldFrame;
+
+    Player(int Width, int Height, TETile[][] world, Random random) {
+        worldWidth = Width;
+        worldHeight = Height;
+        worldFrame = world;
+        r = random;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public TETile[][] addPlayer() {
+        for (x = (worldWidth / 8 + r.nextInt(worldWidth / 3)); x < worldWidth; x++) {
+            for (y = (worldHeight / 8 + r.nextInt(worldHeight / 3)); y < worldHeight; y++) {
+                if (worldFrame[x][y] == Tileset.FLOOR) {
+                    break;
+                }
+            }
+            if (worldFrame[x][y] == Tileset.FLOOR) {
+                break;
+            }
+        }
+        worldFrame[x][y] = Tileset.AVATAR;
+        initialX = x;
+        initialY = y;
+        return worldFrame;
+    }
+
+
+    public TETile[][] move(char input) {
+        int factor = 0;
+        if (input == 'A' || input == 'a' || input == 'D' || input == 'd') {
+            if (input == 'A' || input == 'a') {
+                factor= -1;
+            } else {
+                factor = 1;
+            }
+            if (worldFrame[x + factor][y] == Tileset.FLOOR) {
+                worldFrame[x][y] = Tileset.FLOOR;
+                x += factor;
+                worldFrame[x][y] = Tileset.AVATAR;
+            }
+        } else if (input == 'S' || input == 's' || input == 'W' || input == 'w') {
+            if (input == 'S' || input == 's') {
+                factor = -1;
+            } else if (input == 'W' || input == 'w') {
+                factor = 1;
+            }
+            if (worldFrame[x][y + factor] == Tileset.FLOOR) {
+                worldFrame[x][y] = Tileset.FLOOR;
+                y += factor;
+                worldFrame[x][y] = Tileset.AVATAR;
+            }
+        }
+        return worldFrame;
+    }
+}
